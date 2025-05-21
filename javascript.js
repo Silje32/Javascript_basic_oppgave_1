093fb67757076af4e83a4c49475bf541539246f9
Silje32 <msh070425@gmail.com>
2025-05-21 14:50:32 +0200

    Changed my variable from let to const

--- javascript.js
+++ javascript.js
@@ -56,20 +56,20 @@ Skriv noen eksempler der du tester disse operatorene.
 
 // Skriv koden fletor oppgave 3 her
 
-let a = 5;
-let b = 20;
+const a = 5;
+const b = 20;
 
 // Addition operator
-console.log(a+b);
+console.log(a + b);
 
 // Subtraction operator
-console.log(a-b);
+console.log(a - b);
 
 // Division operator
-console.log(a/b);
+console.log(a / b);
 
 // Multiplication operator
-console.log(a*b);
+console.log(a * b);
 
 // ++
 //console.log(a++);
